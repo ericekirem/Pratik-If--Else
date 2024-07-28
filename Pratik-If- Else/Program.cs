a1e5614951d54688eebb4b68c3fd7a74751f0d59
ericekirem <irem.ericek@icloud.com>
2024-07-29 01:36:56 +0300

    Add comment lines.

--- Pratik-If- Else/Program.cs
+++ Pratik-If- Else/Program.cs
@@ -1,9 +1,11 @@
 ﻿
 
 Console.Write("Lütfen bir sayı giriniz: ");
+//Take number from user
 int number = int.Parse(Console.ReadLine());
+//Parse number to int
 
-
+//Compare the number with 10
 if (number == 10)
 {
     Console.WriteLine("Girdiğiniz sayı 10'a eşittir.");
@@ -17,7 +19,7 @@ else
     Console.WriteLine("Girdiğiniz sayı 10'dan büyüktür.");
 }
 
-
+//Check if the number is even or odd
 if (number % 2 == 0)
 {
     Console.WriteLine("Girdiğiniz sayı çifttir.");
